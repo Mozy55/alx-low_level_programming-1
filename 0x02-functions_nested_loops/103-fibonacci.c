f2e5840f049b3ba8fa8c8c503828cd877a467c5a
Codesbymeng <a.menguit@gmail.com>
2023-03-22 03:54:15 +0100

    task-15-update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,34 +2,35 @@
 
 /**
  * This is the main function of the program.
- * It initializes variables, calculates the sum of even-valued Fibonacci terms up to 4 million,
+ * It init var, calculates the sum of even-valued Fibonacci terms up to 4M,
  * and prints the result to the console.
  *
  * @return 0 on successful program execution.
  */
 int main(void)
 {
-    int i;
-    unsigned long int j, k, next, sum;
+int i;
+unsigned long int j, k, next, sum;
 
-    j = 1;
-    k = 2;
-    sum = 0;
+j = 1;
+k = 2;
+sum = 0;
 
-    for (i = 1; i <= 33; ++i)
-    {
-        if (j < 4000000 && (j % 2) == 0)
-        {
-            sum = sum + j;
-        }
-        next = j + k;
-        j = k;
-        k = next;
-    }
-
-    printf("%lu\n", sum);
-
-    return 0;
+for (i = 1; i <= 33; ++i)
+{
+if (j < 4000000 && (j % 2) == 0)
+{
+sum = sum + j;
 }
+next = j + k;
+j = k;
+k = next;
+}
+
+printf("%lu\n", sum);
+
+return 0;
+}
+
 
 
