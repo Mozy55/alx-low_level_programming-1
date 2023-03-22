36aaff819f49180347df4e316697009bffb4ce02
Codesbymeng <a.menguit@gmail.com>
2023-03-22 03:11:23 +0100

    task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Entry point
+ * Description: adds two integers and returns the result.
+ * @i: character to be added.
+ * @j: character to be added.
+ * Return: the value of the last digit.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
